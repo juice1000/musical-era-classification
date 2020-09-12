4964e67405160c65b72a0dd063d5ae19e0735ce4
juice1000 <julienlook@gmx.de>
2020-09-12 21:26:42 +0200

    spotify.py -- fixed bugs, downloads completed

--- spotify.py
+++ spotify.py
@@ -5,9 +5,6 @@ import json
 import pandas as pd
 import os
 import time
-import sys
-# client_credentials_manager = SpotifyClientCredentials(client_id='54372e1b817d4af19982352a52541a48', client_secret='ef3cdce4889e4a55aff19bebbdaa5c24')
-
 
 
 def dict_iterator(d, song_title):
@@ -42,21 +39,11 @@ if token:
     sp = spotipy.Spotify(auth=token)
     results = sp.current_user_saved_tracks()
     auth = "Authorization: Bearer %s" % token
-    #for item in results['items']:
-     #   track = item['track']
-      #  print(track['name'] + ' - ' + track['artists'][0]['name'])
 
-    # url = 'https://p.scdn.co/mp3-preview/9a0ddfe0e2598a940f2d87bdfce9a6281137c8ab?cid=774b29d4f13844c495f206cafdad9c86'
-    # bearer = 'Bearer %s' %token
-    # headers = {'Authorization' : bearer}
-    # print(headers)
-    # r = requests.get(url, headers=headers)
     df = pd.read_csv("data_library/url_data.csv")
     df = df.values
-    #print(df)
 
     counter = 0
-    #subprocess.call(['curl', 'https://p.scdn.co/mp3-preview/3205d45d73576078c8c31d55e8691c1c31aa40f2?cid=54372e1b817d4af19982352a52541a48', '-H', 'Authorization: Bearer BQAmTF06iTr3Q64p4BUkyLgwTMfaHMGxQDjgH6L1dyi7iQIk2m6eAt0Fyt3auldOSbkxIecNDE6BaXK1EKrzlzmR5zqgF6XDVqEdJT3W-ofiM942LGrDhbi48aGcAImzrGKwjzL2rATXZy6ah1Lg9TZfyw-g', '--output', 'music_library/Frankie Yankovic/Walter Ostanek -- Blue Skirt Waltz.mp3'])
     for i in range(len(df)):
 
         song_url = df[i][4]
@@ -95,9 +82,3 @@ if token:
 
 else:
     print("Can't get token for", username)
-
-# token = credentials.get_access_token()
-# spotify = spotipy.Spotify(auth=token)
-
-
-# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
