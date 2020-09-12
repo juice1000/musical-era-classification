5a93782542418358b4b3bd93b2da3362c28b46d2
juice1000 <julienlook@gmx.de>
2020-09-12 21:25:55 +0200

    spotify.py -- fixed bugs, downloads completed

--- spotify.py
+++ spotify.py
@@ -56,15 +56,15 @@ if token:
     #print(df)
 
     counter = 0
-
+    #subprocess.call(['curl', 'https://p.scdn.co/mp3-preview/3205d45d73576078c8c31d55e8691c1c31aa40f2?cid=54372e1b817d4af19982352a52541a48', '-H', 'Authorization: Bearer BQAmTF06iTr3Q64p4BUkyLgwTMfaHMGxQDjgH6L1dyi7iQIk2m6eAt0Fyt3auldOSbkxIecNDE6BaXK1EKrzlzmR5zqgF6XDVqEdJT3W-ofiM942LGrDhbi48aGcAImzrGKwjzL2rATXZy6ah1Lg9TZfyw-g', '--output', 'music_library/Frankie Yankovic/Walter Ostanek -- Blue Skirt Waltz.mp3'])
     for i in range(len(df)):
 
         song_url = df[i][4]
         song_title = df[i][3]
         file_title = df[i][2] + " -- " + df[i][3]
         json_file = subprocess.check_output(["curl", "-X", "GET", song_url, "-H", auth])
-
-        if str(json_file) != "b''":
+        print(json_file)
+        if str(json_file) != "b''" and str(json_file).find("Error") == -1 and str(json_file).find("error") == -1 :
             dictionary = json.loads(json_file)
             dictionary = dictionary['tracks']['items']
 
@@ -83,7 +83,7 @@ if token:
 
 
             if download_url != "":
-                subprocess.check_output(["curl", download_url, "-H", auth, "--output", path])
+                subprocess.call(["curl", download_url, "-H", auth, "--output", path])
             else:
                 "DOWNLOAD URL COULD NOT BE FETCHED"
 
@@ -91,8 +91,7 @@ if token:
             print(counter)
         else:
             print("ERROR RETRIEVING URL...")
-            time.sleep(2)
-################# DOWNLOADING STILL NOT WORKING ############################
+            time.sleep(1)
 
 else:
     print("Can't get token for", username)
