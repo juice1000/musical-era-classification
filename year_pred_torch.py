4079a941327f8344826430276efb26f516967f50
juice1000 <julienlook@gmx.de>
2020-10-19 16:41:53 +0200

    enable multiprocessing for year_pred_torch

--- year_pred_torch.py
+++ year_pred_torch.py
@@ -19,6 +19,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
 from torch.utils.data import DataLoader, TensorDataset
+import torch.multiprocessing as mp
+
 
 save_dir = 'saved_models'
 device = "cpu"
@@ -45,16 +47,23 @@ def get_dataloader(x: np.array, y: np.array, batch_size: int, shuffle: bool = Tr
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, length, width):
         super().__init__()
-        self.model = nn.Sequential(
-            nn.Linear(36, 200),
-            nn.ReLU(),
-            nn.Linear(200, 200),
-            nn.ReLU(),
-            nn.Linear(200, 5),
-            nn.LogSoftmax(),
-        )
+
+        self.width = width
+        self.length = length
+
+        modules = []
+        modules.append(nn.Linear(36, self.width))
+        modules.append(nn.ReLU())
+        for i in range(self.length):
+            modules.append(nn.Linear(self.width, self.width))
+            modules.append(nn.ReLU())
+        modules.append(nn.Linear(self.width, 5))
+        modules.append(nn.Softmax(dim=0))
+
+        self.model = nn.Sequential(*modules)
+
         for m in self.model:
             if isinstance(m, nn.Linear):
                 nn.init.kaiming_normal_(m.weight)
@@ -64,22 +73,13 @@ class Net(nn.Module):
         return self.model(x)
 
 
-
 def data_preprocessing(filename):
     print("GETTING DATASET")
-
-    data = pd.read_csv(filename)
-    if "Unnamed: 0" in data.columns:
-        data = data.drop(columns="Unnamed: 0")
-    data = data.replace([np.inf, -np.inf], np.nan).dropna()
-    #print(data.head())
-
+    data = filename
     dataset = data.values
-
 
     training_examples = dataset[:,1:]
     training_labels = dataset[:, 0]
-
 
     training_examples = preprocessing.scale(training_examples)
 
@@ -110,177 +110,202 @@ def data_preprocessing(filename):
     X = training_examples
     Y = training_labels
 
-    return X, Y, features
+    return X, Y
 
 
-def main(training_data, test_data):
+class SharedAdam(torch.optim.Adam):
+    def __init__(self, params, lr=1e-4, betas=(0.9, 0.99), eps=1e-8,
+                 weight_decay=0):
+        super(SharedAdam, self).__init__(params, lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        # State initialization
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'] = 0
+                state['exp_avg'] = torch.zeros_like(p.data)
+                state['exp_avg_sq'] = torch.zeros_like(p.data)
 
-        # We test for 100 - 400 nodes
-        # With 2-5 hidden layers
-        start = datetime.datetime.now()
-
-        df = pd.DataFrame(index=["1 Hidden", "2 Hidden", "3 Hidden"], columns=["100", "200", "300", "400", "500"])
-
-
-        for i in range(1, 6):
-            width = i * 100
-
-            for length in range(2, 5):
-
-                X, Y, features = data_preprocessing(training_data)
-
-                #kf = StratifiedKFold(n_splits=8, random_state=3829, shuffle=True)
-                # CREATE NEW MODEL
-                net = Net()
-
-                optimizer = torch.optim.Adam(
-                    net.parameters(), betas=(0.9, 0.999), lr=1e-4, weight_decay=0)
-                criterion = nn.NLLLoss()
-
-                #model = CNN_model(width=width, length=length, features=features, test=False)
-                #es = EarlyStopping(monitor='loss', mode='min', verbose=1, patience=10)
-                #mc = ModelCheckpoint(get_model_name(), monitor='accuracy', mode='max', verbose=0, save_best_only=True)
-                #callbacks = [es, mc]
-
-                # fix random seed for reproducibility
-                seed = 7
-                np.random.seed(seed)
-                kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
-                fold_var = 1
+                # share in memory
+                state['exp_avg'].share_memory_()
+                state['exp_avg_sq'].share_memory_()
 
 
-                for train, test in kfold.split(X, Y):
+class Worker(mp.Process):
+    def __init__(self, gnet, opt, global_ep, name, shards, criterion, width, length):
+        super(Worker, self).__init__()
+        self.name = 'w%02i' % name
+        self.g_ep = global_ep
+        self.gnet, self.optimizer = gnet, opt
+        self.width = width
+        self.length = length
+        self.lnet = Net(self.length, self.width)  # local network
+        self.shards = shards
+        self.criterion = criterion
 
-                    cvscores = []
-                    cvloss = []
+    def run(self):
 
+        X, Y = data_preprocessing(self.shards)
 
-##################################################
+        while self.g_ep.value <= len(Y):
 
-                    train_loader = get_dataloader(
-                        X[train], Y[train],
-                        batch_size=2000, shuffle=True
-                    )
-                    val_loader = get_dataloader(
-                        X[test], Y[test],
-                        batch_size=100, shuffle=False
-                    )
+            start = datetime.datetime.now()
+
+            # fix random seed for reproducibility
+            seed = 7
+            np.random.seed(seed)
+            kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+            fold_var = 1
+
+            cvloss = []
+            for train, test in kfold.split(X, Y):
 
 
 
-                    for i, data in enumerate(train_loader, 0):
-                        net.train()
+                ##################################################
 
-                        inputs, labels = data
-                        labels = labels.to(device, dtype=torch.int64)
-
-                        # zero the parameter gradients
-                        optimizer.zero_grad()
-
-                        # forward + backward + optimize
-                        outputs = net(inputs)
-                        loss = criterion(outputs, labels)
-                        loss.backward()
-                        optimizer.step()
-
-                        with torch.no_grad():
-                            net.eval()
-
-                            for i, data in enumerate(val_loader, 0):
-                                inputs, labels = data
-                                labels = labels.to(device, dtype=torch.int64)
-
-                                outputs_val = net(inputs)
-                                val_loss = criterion(outputs_val, labels)
-                                #loss.backward()
-                                #optimizer.step()
-                                cvloss.append(val_loss.item())
-
-                    print("LOSS: ", np.mean(cvloss))
-
-                    #model.save_weights(get_model_name())
-                    fold_var += 1
-
-
-
-                #print("\n%%%%%% CREATE PLOTS %%%%%%")
-
-                #plt.figure(1)
-                #plt.plot(cvscores)
-                #plt.title('model accuracy')
-                #plt.ylabel('accuracy')
-                #plt.title("Training -- Accuracy")
-
-                #plt.figure(2)
-                #plt.plot(cvloss)
-                #plt.title("Loss")
-                #plt.ylabel("Loss")
-                #plt.title("Training -- Loss")
-
-                end = datetime.datetime.now()
-                print("Elapsed Time: ", ((end-start).total_seconds())/60)
-
-
-    #######################    TESTING     ########################
-
-                X, Y, features = data_preprocessing(test_data)
-                test_loader = get_dataloader(
-                    X, Y,
-                    batch_size=200, shuffle=False
+                train_loader = get_dataloader(
+                    X[train], Y[train],
+                    batch_size=2000, shuffle=True
                 )
-                predictions, val_losses = [], []
-                #model.load_weights(get_model_name())
-                #y_test = keras.utils.to_categorical(Y, num_classes=features)
+                val_loader = get_dataloader(
+                    X[test], Y[test],
+                    batch_size=100, shuffle=False
+                )
 
-                count = 0
-                labelsize = 0
+                for i, data in enumerate(train_loader, 0):
+                    self.lnet.train()
 
-                with torch.no_grad():
-                    net.eval()
-                    for i, data in enumerate(test_loader, 0):
-                        inputs, labels = data
-                        labels_ = labels.to(device, dtype=torch.int64)
+                    inputs, labels = data
+                    labels = labels.to(device, dtype=torch.int64)
 
-                        predictions = net(inputs)
-                        test_loss = criterion(predictions, labels_)
+                    # zero the parameter gradients
+                    self.optimizer.zero_grad()
 
-                        _, predicted = torch.max(predictions.data, 1)
-                        labelsize += labels.size(0)
-                        count += (predicted == labels).sum().item()
-
-
-                    accuracy = 100 * count / labelsize
-                    print('TEST ACCURACY: ', (accuracy))
-                    print('TEST LOSS: %d %%' % (test_loss))
-
-                #predictions = model.predict(X)
-                #predictions = np.array(predictions)
-                #le = preprocessing.LabelBinarizer()
-                #le.fit([0, 1, 2, 3, 4, 5, 6, 7, 8])
-                #predictions = le.inverse_transform(predictions)
-                #print(predictions)
-
-                #m = tf.keras.metrics.Accuracy()
-                #m.update_state(Y, predictions)
-                #print("\n\nMODEL ACCURACY - TEST: ", round(m.result().numpy(), 4) * 100, "%" )
+                    # forward + backward + optimize
+                    outputs = self.lnet(inputs)
+                    loss = self.criterion(outputs, labels)
+                    loss.backward()
+                    self.optimizer.step()
 
 
-                #scores = model.evaluate(X, y_test, verbose=0)
-                #print("EVALUATION - %s: %.2f%%, loss:" % (model.metrics_names[1], scores[1] * 100), scores[0])
-                #cvscores.append(scores[1] * 100)
-                #cvloss.append(scores[0])
 
-                df.loc["{} Hidden".format(length-1), str(width)] = accuracy
+                    for lp, gp in zip(self.lnet.parameters(), self.gnet.parameters()):
+                        gp._grad = lp.grad
+                    self.optimizer.step()
+                    self.lnet.load_state_dict(self.gnet.state_dict())
 
-        df.to_csv("NN_eval/NN_eval_librosa_audiofeatures.csv")
-        #plt.show()
+                    with self.g_ep.get_lock():
+                        self.g_ep.value += 1
+
+                    with torch.no_grad():
+                        self.lnet.eval()
+
+                        for i, data in enumerate(val_loader, 0):
+                            inputs, labels = data
+                            labels = labels.to(device, dtype=torch.int64)
+
+                            outputs_val = self.lnet(inputs)
+                            val_loss = self.criterion(outputs_val, labels)
+                            cvloss.append(val_loss.item())
+
+                            with self.g_ep.get_lock():
+                                self.g_ep.value += 1
+
+                fold_var += 1
+            print("LOSS: ", np.mean(cvloss))
+
+
+                # model.save_weights(get_model_name())
+
+            end = datetime.datetime.now()
+            print("\nWorker: ", self.name, " | Elapsed Time: ", ((end - start).total_seconds()) / 60)
+
+
 
 
 if __name__ == "__main__":
 
     print("\n INITIALIZE TRAINING")
 
-    main(training_data="data_library/librosa_audiofeatures_train5feat.csv",
-         test_data="data_library/librosa_audiofeatures_test5feat.csv")
+    # We test for 100 - 400 nodes
+    # With 2-5 hidden layers
+    df = pd.DataFrame(index=["1 Hidden", "2 Hidden", "3 Hidden"], columns=["100", "200", "300", "400", "500"])
+
+    training_data = "data_library/librosa_audiofeatures_train5feat.csv"
+    data = pd.read_csv(training_data)
+    if "Unnamed: 0" in data.columns:
+        data = data.drop(columns="Unnamed: 0")
+    #data = data.replace([np.inf, -np.inf], np.nan).dropna()
+
+    # create as many processes as there are CPUs on your machine
+    num_processes = mp.cpu_count()
+    # calculate the chunk size as an integer
+    shard_size = int(data.shape[0] / num_processes)
+    # this solution was reworked from the above link.
+    # will work even if the length of the dataframe is not evenly divisible by num_processes
+    shards = [data.iloc[data.index[i:i + shard_size]] for i in range(0, data.shape[0], shard_size)]
+
+    for i in range(1, 6):
+        width = i * 100
+
+        for length in range(2, 5):
+            gnet = Net(length, width)
+            print(gnet)
+            criterion = nn.CrossEntropyLoss()
+
+            gnet.share_memory()  # share the global parameters in multiprocessing
+            opt = SharedAdam(gnet.parameters(), lr=0.005, betas=(0.92, 0.999))  # global optimizer
+            global_ep, global_ep_r = mp.Value('i', 0), mp.Value('d', 0.)
+
+            workers = [Worker(gnet, opt, global_ep, i, shards[i], criterion, width, length)
+            for i in range(mp.cpu_count())]
+            [w.start() for w in workers]
+
+            [w.join() for w in workers]
+
+            #######################    TESTING     ########################
+
+            test_data = pd.read_csv("data_library/librosa_audiofeatures_test5feat.csv")
+            if "Unnamed: 0" in test_data.columns:
+                test_data = test_data.drop(columns="Unnamed: 0")
+
+            X_test, Y_test = data_preprocessing(test_data)
+
+            test_loader = get_dataloader(
+                X_test, Y_test,
+                batch_size=200, shuffle=False
+            )
+
+            count = 0
+            labelsize = 0
+            test_loss = []
+            scores = []
+
+            with torch.no_grad():
+                gnet.eval()
+                for i, data in enumerate(test_loader, 0):
+                    inputs, labels = data
+                    labels_ = labels.to(device, dtype=torch.int64)
+
+                    predictions = gnet(inputs)
+                    loss = criterion(predictions, labels_)
+                    test_loss.append(loss.item())
+
+                    _, predicted = torch.max(predictions.data, 1)
+                    labelsize += labels.size(0)
+                    count += (predicted == labels).sum().item()
+
+                accuracy = round((100 * count / labelsize), 2)
+                final_loss = round((np.mean(test_loss)),2)
+                print('\n\nTEST ACCURACY: ', accuracy, "%")
+                print('TEST LOSS: ', final_loss)
+
+                scores.append(accuracy)
+                scores.append(test_loss)
+
+            df.loc["{} Hidden".format(length - 1), str(width)] = scores
+
+    df.to_csv("NN_eval/NN_eval_librosa_audiofeatures.csv")
+
 
 
